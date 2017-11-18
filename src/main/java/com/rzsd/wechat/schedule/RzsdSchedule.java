6be23d73c04062a69541fb2efdc9050d28284272
ZhengChao <zhengc@liandisys.com.cn>
2017-11-19 03:50:13 +0900

    自动更新状态

--- src/main/java/com/rzsd/wechat/schedule/RzsdSchedule.java
+++ src/main/java/com/rzsd/wechat/schedule/RzsdSchedule.java
@@ -16,11 +16,35 @@ public class RzsdSchedule {
     @Autowired
     private InvoiceService invoiceServiceImpl;
 
-    @Scheduled(cron = "0 43 2 * * ?")
-    public void UpdateStatus() {
+    @Scheduled(cron = "0 0 * * * ?")
+    public void UpdateStatus1() {
         LOGGER.info("状态更新开始：出库包裹---日本海关仓库");
         invoiceServiceImpl.doUpdateDetailStatus(InvoiceDetailStatus.CHUKU.getCode(),
                 InvoiceDetailStatus.RBHGCK.getCode());
         LOGGER.info("状态更新结束：出库包裹---日本海关仓库");
+    }
+
+    @Scheduled(cron = "0 5 * * * ?")
+    public void UpdateStatus2() {
+        LOGGER.info("状态更新开始：日本海关仓库---日本海关清关完成");
+        invoiceServiceImpl.doUpdateDetailStatus(InvoiceDetailStatus.RBHGCK.getCode(),
+                InvoiceDetailStatus.RBHGQGWC.getCode());
+        LOGGER.info("状态更新结束：日本海关仓库---日本海关清关完成");
+    }
+
+    @Scheduled(cron = "0 10 * * * ?")
+    public void UpdateStatus3() {
+        LOGGER.info("状态更新开始：日本海关清关完成---转机起飞");
+        invoiceServiceImpl.doUpdateDetailStatus(InvoiceDetailStatus.RBHGQGWC.getCode(),
+                InvoiceDetailStatus.ZJQF.getCode());
+        LOGGER.info("状态更新结束：日本海关清关完成---转机起飞");
+    }
+
+    @Scheduled(cron = "0 15 * * * ?")
+    public void UpdateStatus4() {
+        LOGGER.info("状态更新开始：转机起飞---到达国内海关仓库");
+        invoiceServiceImpl.doUpdateDetailStatus(InvoiceDetailStatus.ZJQF.getCode(),
+                InvoiceDetailStatus.GNHGCK.getCode());
+        LOGGER.info("状态更新结束：转机起飞---到达国内海关仓库");
     }
 }
