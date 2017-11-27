9eb3ceaeadcfe41dd965084c48db7d0a21a1f33d
ZhengChao <zhengc@liandisys.com.cn>
2017-11-27 22:24:48 +0900

    创建账号对应

--- src/main/java/com/rzsd/wechat/logic/impl/WechatQuickLogicImpl.java
+++ src/main/java/com/rzsd/wechat/logic/impl/WechatQuickLogicImpl.java
@@ -23,6 +23,7 @@ import com.rzsd.wechat.context.ChatContextInstance;
 import com.rzsd.wechat.logic.WechatCustomIdLogic;
 import com.rzsd.wechat.logic.WechatInvoiceLogic;
 import com.rzsd.wechat.logic.WechatQuickLogic;
+import com.rzsd.wechat.logic.WechatUserLogic;
 import com.rzsd.wechat.util.CheckUtil;
 import com.rzsd.wechat.util.DateUtil;
 import com.rzsd.wechat.util.InputMessage;
@@ -39,11 +40,15 @@ public class WechatQuickLogicImpl implements WechatQuickLogic {
     private static final String TYPE_QUERY_10 = "5";
     private static final String TYPE_QUERY_ADDRESS = "6";
     private static final String TYPE_EDIT_ADDRESS = "7";
+    private static final String TYPE_CREATE_ACCOUNT = "8";
+    private static final String TYPE_RESET_PASSWORD = "9";
 
     @Autowired
     private WechatCustomIdLogic wechatCustomIdLogicImpl;
     @Autowired
     private WechatInvoiceLogic wechatInvoiceLogicImpl;
+    @Autowired
+    private WechatUserLogic wechatUserLogicImpl;
 
     @Autowired
     private MUserMapper mUserMapper;
@@ -210,6 +215,39 @@ public class WechatQuickLogicImpl implements WechatQuickLogic {
             String msg = WechatMessageUtil.getTextMessage("text.quick.hint.custominfo.edit.selectcd",
                     inputMsg.getFromUserName(), inputMsg.getToUserName(), sb.toString());
             response.getOutputStream().write(msg.getBytes("UTF-8"));
+            return true;
+        }
+
+        if (TYPE_CREATE_ACCOUNT.equals(inputMsg.getContent())) {
+            ChatContextInstance.newInstance(openId);
+            ChatContextInstance.setType(openId, inputMsg.getContent());
+            String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.hint.name", inputMsg.getFromUserName(),
+                    inputMsg.getToUserName());
+            response.getOutputStream().write(msg.getBytes("UTF-8"));
+            LOGGER.debug(msg);
+            return true;
+        }
+
+        if (TYPE_RESET_PASSWORD.equals(inputMsg.getContent())) {
+            MUser selectCond = new MUser();
+            selectCond.setWechatOpenId(openId);
+            List<MUser> userList = mUserMapper.select(selectCond);
+
+            if (userList.isEmpty()) {
+                // 用户已经注册网站账号
+                String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.resetpwdnotregisted",
+                        inputMsg.getFromUserName(), inputMsg.getToUserName());
+                response.getOutputStream().write(msg.getBytes("UTF-8"));
+                ChatContextInstance.removeInstance(openId);
+                return true;
+            }
+
+            ChatContextInstance.newInstance(openId);
+            ChatContextInstance.setType(openId, inputMsg.getContent());
+            String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.hint.resetpwd",
+                    inputMsg.getFromUserName(), inputMsg.getToUserName());
+            response.getOutputStream().write(msg.getBytes("UTF-8"));
+            LOGGER.debug(msg);
             return true;
         }
 
@@ -411,6 +449,213 @@ public class WechatQuickLogicImpl implements WechatQuickLogic {
             LOGGER.debug(msg);
             return true;
 
+        }
+
+        if (TYPE_EDIT_ADDRESS.equals(chatContextDto.getType())) {
+            //
+            if (StringUtils.isEmpty(chatContextDto.getWord1())) {
+                if (inputMsg.getContent().length() == 5
+                        && CheckUtil.isValidString(inputMsg.getContent().substring(0, 4), CheckUtil.REGEX_ALPHABETA)
+                        && CheckUtil.isValidString(inputMsg.getContent().substring(4), CheckUtil.REGEX_NUM)) {
+                    MUser selectCond = new MUser();
+                    selectCond.setWechatOpenId(openId);
+                    List<MUser> userList = mUserMapper.select(selectCond);
+
+                    if (userList.isEmpty()
+                            || !userList.get(0).getCustomId().equalsIgnoreCase(inputMsg.getContent().substring(0, 4))) {
+                        // 编码不正确
+                        String msg = WechatMessageUtil.getTextMessage("text.quick.hint.custominfo.edit.selectcd.error",
+                                inputMsg.getFromUserName(), inputMsg.getToUserName());
+                        response.getOutputStream().write(msg.getBytes("UTF-8"));
+                        return true;
+                    }
+
+                    MCustomInfo mCustomInfoCond = new MCustomInfo();
+                    mCustomInfoCond.setCustomId(userList.get(0).getCustomId());
+                    mCustomInfoCond.setRowNo(inputMsg.getContent().substring(4).toUpperCase());
+                    List<MCustomInfo> mCustomInfoLst = mCustomInfoMapper.select(mCustomInfoCond);
+                    if (mCustomInfoLst.isEmpty()) {
+                        // 编码不正确
+                        String msg = WechatMessageUtil.getTextMessage("text.quick.hint.custominfo.edit.selectcd.error",
+                                inputMsg.getFromUserName(), inputMsg.getToUserName());
+                        response.getOutputStream().write(msg.getBytes("UTF-8"));
+                        return true;
+                    }
+
+                    ChatContextInstance.setWord1(openId,
+                            mCustomInfoLst.get(0).getCustomId() + mCustomInfoLst.get(0).getRowNo());
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.hint.custominfo.edit.newname",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+
+                // 客户编码ID不正确
+                String msg = WechatMessageUtil.getTextMessage("text.quick.hint.custominfo.edit.selectcd.error",
+                        inputMsg.getFromUserName(), inputMsg.getToUserName());
+                response.getOutputStream().write(msg.getBytes("UTF-8"));
+                return true;
+
+            }
+            if (StringUtils.isEmpty(chatContextDto.getWord2())) {
+                if (!CheckUtil.isLengthValid(inputMsg.getContent(), 10)) {
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.hint.name.lenerr",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+                ChatContextInstance.setWord2(openId, inputMsg.getContent());
+                String msg = WechatMessageUtil.getTextMessage("text.quick.hint.telno", inputMsg.getFromUserName(),
+                        inputMsg.getToUserName(), inputMsg.getContent());
+                response.getOutputStream().write(msg.getBytes("UTF-8"));
+                LOGGER.debug(msg);
+                return true;
+            }
+            if (StringUtils.isEmpty(chatContextDto.getWord3())) {
+                if (!CheckUtil.isValidString(inputMsg.getContent(), CheckUtil.REGEX_NUM)) {
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.hint.telno.numerr",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    LOGGER.debug(msg);
+                    return true;
+                }
+
+                if (!CheckUtil.isLengthValid(inputMsg.getContent(), 13)) {
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.hint.telno.lenerr",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    LOGGER.debug(msg);
+                    return true;
+                }
+                ChatContextInstance.setWord3(openId, inputMsg.getContent());
+                String msg = WechatMessageUtil.getTextMessage("text.quick.hint.address", inputMsg.getFromUserName(),
+                        inputMsg.getToUserName(), chatContextDto.getWord1());
+                response.getOutputStream().write(msg.getBytes("UTF-8"));
+                LOGGER.debug(msg);
+                return true;
+            }
+            if (StringUtils.isEmpty(chatContextDto.getWord4())) {
+                if (!CheckUtil.isLengthValid(inputMsg.getContent(), 64)) {
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.hint.address.lenerr",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    LOGGER.debug(msg);
+                    return true;
+                }
+                ChatContextInstance.setWord4(openId, inputMsg.getContent());
+                String cmd = "修改地址 " + chatContextDto.getWord1().replace(" ", "") + " "
+                        + chatContextDto.getWord2().replace(" ", "") + " " + chatContextDto.getWord3().replace(" ", "")
+                        + " " + chatContextDto.getWord4().replace(" ", "");
+                inputMsg.setContent(cmd);
+                wechatCustomIdLogicImpl.updateCustomInfo(inputMsg, response);
+                ChatContextInstance.removeInstance(openId);
+                return true;
+            }
+
+            ChatContextInstance.newInstance(openId);
+            String msg = WechatMessageUtil.getTextMessage("text.quick.hint.error", inputMsg.getFromUserName(),
+                    inputMsg.getToUserName());
+            response.getOutputStream().write(msg.getBytes("UTF-8"));
+            LOGGER.debug(msg);
+            return true;
+
+        }
+
+        if (TYPE_CREATE_ACCOUNT.equals(chatContextDto.getType())) {
+            // 创建账号
+            if (StringUtils.isEmpty(chatContextDto.getWord1())) {
+                if (!CheckUtil.isValidString(inputMsg.getContent(), CheckUtil.REGEX_ALPHABETA_NUM)) {
+                    // 格式不正确
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.nameformat",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+
+                MUser selectCond = new MUser();
+                selectCond.setWechatOpenId(openId);
+                List<MUser> userList = mUserMapper.select(selectCond);
+
+                if (!userList.isEmpty()) {
+                    // 用户已经注册网站账号
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.registed",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    ChatContextInstance.removeInstance(openId);
+                    return true;
+                }
+
+                selectCond = new MUser();
+                selectCond.setUserName(inputMsg.getContent());
+                userList = mUserMapper.select(selectCond);
+
+                if (!userList.isEmpty()) {
+                    // 用户已经注册网站账号
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.nameexisted",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+
+                ChatContextInstance.setWord1(openId, inputMsg.getContent());
+                String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.hint.password",
+                        inputMsg.getFromUserName(), inputMsg.getToUserName());
+                response.getOutputStream().write(msg.getBytes("UTF-8"));
+                return true;
+
+            }
+            if (StringUtils.isEmpty(chatContextDto.getWord2())) {
+                if (CheckUtil.isLengthValid(inputMsg.getContent(), 6)) {
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.pwdlen",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+                if (!CheckUtil.isValidString(inputMsg.getContent(), CheckUtil.REGEX_ALPHABETA_NUM)) {
+                    // 格式不正确
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.pwdformat",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+                String cmd = "创建账号 " + chatContextDto.getWord1().replace(" ", "") + " 日中 "
+                        + chatContextDto.getWord2().replace(" ", "");
+                inputMsg.setContent(cmd);
+                wechatUserLogicImpl.createUser(inputMsg, response);
+                ChatContextInstance.removeInstance(openId);
+                return true;
+            }
+
+            ChatContextInstance.newInstance(openId);
+            String msg = WechatMessageUtil.getTextMessage("text.quick.hint.error", inputMsg.getFromUserName(),
+                    inputMsg.getToUserName());
+            response.getOutputStream().write(msg.getBytes("UTF-8"));
+            LOGGER.debug(msg);
+            return true;
+
+        }
+
+        if (TYPE_RESET_PASSWORD.equals(chatContextDto.getType())) {
+            if (StringUtils.isEmpty(chatContextDto.getWord1())) {
+                if (CheckUtil.isLengthValid(inputMsg.getContent(), 6)) {
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.pwdlen",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+                if (!CheckUtil.isValidString(inputMsg.getContent(), CheckUtil.REGEX_ALPHABETA_NUM)) {
+                    // 格式不正确
+                    String msg = WechatMessageUtil.getTextMessage("text.quick.createacc.err.pwdformat",
+                            inputMsg.getFromUserName(), inputMsg.getToUserName());
+                    response.getOutputStream().write(msg.getBytes("UTF-8"));
+                    return true;
+                }
+                String cmd = "重置密码 " + chatContextDto.getWord1().replace(" ", "");
+                inputMsg.setContent(cmd);
+                wechatUserLogicImpl.updatePassword(inputMsg, response);
+                ChatContextInstance.removeInstance(openId);
+                return true;
+            }
         }
 
         return true;
